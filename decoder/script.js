10069ed3bb54a412d729aff5bb742c440dca1694
Samura Hajiyeva (shajiyev) <icosmvs@gmail.com>
2020-07-02 11:20:17 +0200

    enhance btoa

--- decoder/script.js
+++ decoder/script.js
@@ -15,9 +15,69 @@ function clearAllFields() {
 }
 
 function b64DecodeUnicode(str) {
-  return decodeURIComponent(Array.prototype.map.call(atob(str), function (c) {
-    return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2)
-  }).join(''))
+  return urlBase64Decode(str)
+}
+
+function urlBase64Decode(str) { // From https://jwt.io/js/jwt.js
+  var output = str.replace(/-/g, '+').replace(/_/g, '/');
+  switch (output.length % 4) {
+    case 0:
+      break;
+    case 2:
+      output += "==";
+      break;
+    case 3:
+      output += "=";
+      break;
+    default:
+      var b64 =
+          "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
+        b64re = /^(?:[A-Za-z\d+\/]{4})*?(?:[A-Za-z\d+\/]{2}(?:==)?|[A-Za-z\d+\/]{3}=?)?$/;
+
+      window.atob = function (string) {
+        // atob can work with strings with whitespaces, even inside the encoded part,
+        // but only \t, \n, \f, \r and ' ', which can be stripped.
+        string = String(string).replace(/[\\\t\n\f\r ]+/g, "");
+        if (!b64re.test(string))
+          throw new TypeError(
+            "Failed to execute 'atob' on 'Window': The string to be decoded is not correctly encoded."
+          );
+
+        // Adding the padding if missing
+        string += "==".slice(2 - (string.length & 3));
+        var bitmap,
+          result = "",
+          r1,
+          r2,
+          i = 0;
+        for (; i < string.length; ) {
+          bitmap =
+            (b64.indexOf(string.charAt(i++)) << 18) |
+            (b64.indexOf(string.charAt(i++)) << 12) |
+            ((r1 = b64.indexOf(string.charAt(i++))) << 6) |
+            (r2 = b64.indexOf(string.charAt(i++)));
+
+          result +=
+            r1 === 64
+              ? String.fromCharCode((bitmap >> 16) & 255)
+              : r2 === 64
+              ? String.fromCharCode((bitmap >> 16) & 255, (bitmap >> 8) & 255)
+              : String.fromCharCode(
+                  (bitmap >> 16) & 255,
+                  (bitmap >> 8) & 255,
+                  bitmap & 255
+                );
+        }
+        return result;
+      };
+  }
+  var result = window.atob(output); //polifyll https://github.com/davidchambers/Base64.js
+  try{
+    return decodeURIComponent(escape(result));
+  } catch (err) {
+    console.log(err);
+    return result;
+  }
 }
 
 function base16decoder(hexstr) {
@@ -41,7 +101,6 @@ base64Button.addEventListener('click', () => {
 
   if(contents.length) {
     let content  = textarea.value;
-    console.log(content)
     box1.innerHTML = b64DecodeUnicode(content);
     box2.value = b64DecodeUnicode(textarea.value).replace(/[^\x00-\x7F]/g, "");
   } else  {
@@ -70,8 +129,8 @@ container.addEventListener("click", function () {
 
   try {
     var successful = document.execCommand('copy');
-    var msg = successful ? 'successful' : 'unsuccessful';
-    console.log('copy text command was ' + msg);
+    var msg = successful ? 'success' : 'failed';
+    console.log('copy ' + msg);
   } catch (err) {
     console.log('Oops, unable to copy');
   }
